f50bd97eb047a4bb472cf3b1f862a11cae2ba4eb
ScarletFlash <scarletflash.dev@gmail.com>
2024-09-23 00:35:05 +0200

    build: generate .dts bundle (with duplicated typings)

--- core/src/core.ts
+++ core/src/core.ts
@@ -1,17 +1,35 @@
-import { Acessor } from './accessor';
-import { InteractiveObjectBuilder } from './interactive-object-builder';
-import { InteractiveObject } from '@consy/declarations';
+import { CommandDefinition, InteractiveObject } from "@consy/declarations";
+import { Acessor } from "./accessor";
+import { InteractiveObjectBuilder } from "./interactive-object-builder";
 
 export class Core<K extends string> {
-  readonly #interactor: InteractiveObjectBuilder = new InteractiveObjectBuilder();
+  readonly #interactor: InteractiveObjectBuilder =
+    new InteractiveObjectBuilder();
 
-  readonly #accessor: Acessor<InteractiveObject, K> = new Acessor<InteractiveObject, K>(window);
+  readonly #accessor: Acessor<InteractiveObject, K> = new Acessor<
+    InteractiveObject,
+    K
+  >(window);
 
-  public mount(key: K): void {
-    this.#accessor.mount(key, this.#interactor.payload);
+  readonly #key: K;
+
+  constructor(key: K) {
+    this.#key = key;
   }
 
-  public unmount(key: K): void {
-    this.#accessor.unmount(key);
+  public mount(): void {
+    this.#accessor.mount(this.#key, this.#interactor.payload);
+  }
+
+  public unmount(): void {
+    this.#accessor.unmount(this.#key);
+  }
+
+  public addCommand(command: CommandDefinition): void {
+    this.#interactor.addCommand(command);
+  }
+
+  public removeCommand(name: string): void {
+    this.#interactor.removeCommand(name);
   }
 }
